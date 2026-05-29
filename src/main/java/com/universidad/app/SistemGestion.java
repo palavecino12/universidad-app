1eb99298459d369d3aa5e942eb9569d1a03f167f
Facundo <facundopalavecino054@gmail.com>
2026-05-29 20:43:58 -0300

    Estructura inicial del proyecto

---
+++ src/main/java/com/universidad/app/SistemGestion.java
@@ -0,0 +1,7 @@
+package com.universidad.app;
+
+public class SistemGestion {
+    public static void main(String[] args) {
+        System.out.println("¡El sistema de la universidad está arrancando!");
+    }
+}
