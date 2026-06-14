b95e89035f60cc93e265aee185f6ccaa622a7817
MisaelPaoloni <misaelalejandropaoloni@gmail.com>
2026-06-14 16:33:11 -0300

    llevo cambios en inscripcion gestion y dao pero incompletos

--- src/main/java/com/universidad/gestores/GestorInscripcion.java
+++ src/main/java/com/universidad/gestores/GestorInscripcion.java
@@ -1,4 +1,81 @@
 package com.universidad.gestores;
 
-public class GestorInscripcion {
+import com.universidad.clases.Alumno;
+import com.universidad.clases.Inscripcion;
+import com.universidad.clases.Materia;
+import com.universidad.dao.InscripcionesDAO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public class GestorInscripcion{
+
+
+    private final InscripcionesDAO inscripcionDAO;
+
+
+    public GestorInscripcion(InscripcionesDAO inscripcionesDAO){
+
+        this.inscripcionDAO = inscripcionesDAO;
+
+    }
+
+
+
+    public void inscribirAlumno(Alumno alumno, Materia materia){
+
+
+        if(materia.tieneCupo()){
+
+
+            String fecha =
+                    LocalDate.now().toString();
+
+
+            Inscripcion inscripcion =
+                    new Inscripcion(
+                            alumno,
+                            materia,
+                            fecha
+                    );
+
+
+            inscripcionDAO.crearInscripcion(inscripcion);
+
+
+            alumno.getMateriasInscriptas()
+                    .add(materia);
+
+
+            materia.getAlumnosInscriptos()
+                    .add(alumno);
+
+
+
+            System.out.println(
+                    "Inscripcion realizada correctamente"
+            );
+
+
+        }else{
+
+
+            System.out.println(
+                    "No hay cupos disponibles"
+            );
+
+        }
+
+    }
+
+
+
+    public List<Inscripcion> listarInscripciones(){
+
+        return inscripcionDAO.listarInscripciones();
+
+    }
+
+
 }
